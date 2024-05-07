8f24f89298d447290c4a9ec30b93963933fd424f
andhanuprakoso <andhanuprakoso@gmail.com>
2024-05-07 11:13:16 +0700

    tugas

---
+++ LCD/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+int SDA_pin = D4;
+int SCL_pin = D3;
+
+void setup() {
+  Wire.begin(SDA_pin, SCL_pin);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Prak PST 2024");
+  lcd.setCursor(0,1);
+  lcd.print("INFORMATIKA");
+
+
+}
+
+void loop() {
+ 
+}
+
